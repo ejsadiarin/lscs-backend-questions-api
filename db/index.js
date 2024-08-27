2b332c00136f1f4ebfc9dcbcb79ad2c59f2d7982
ejsadiarin <ekwtech.container720@passinbox.com>
2024-08-28 02:58:04 +0800

    feat(create): db queries

--- db/index.js
+++ db/index.js
@@ -14,10 +14,17 @@ export const db_pool = createPool({
 
 export const initDBConnection = async () => {
   try {
-    await db_pool.getConnection();
+    const db = await db_pool.getConnection();
+    const QUERY = `CREATE TABLE IF NOT EXISTS questions_table (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        question TEXT NOT NULL,
+        choices JSON NOT NULL,
+        answer TEXT NOT NULL,
+    )`;
+    await db.query(QUERY);
     console.log("Successfully connected to MYSQL DB!");
   } catch (error) {
-    console.error("Cannot connect to MYSQL DB: ", error);
+    console.error("Error occured.", error);
     throw error;
   }
 };
--- db/query.js
+++ db/query.js
@@ -4,7 +4,8 @@ export const questions_getAllQuestions = async () => {
   const QUERY = "SELECT * FROM questions_table";
   try {
     const db = await db_pool.getConnection();
-    const res = db.query(QUERY);
+    const [res] = db.query(QUERY);
+    res.forEach((q) => (q.choices = JSON.parse(q.choices)));
     return res;
   } catch (error) {
     console.error(
@@ -34,7 +35,40 @@ export const questions_create = async (question, choices, answer) => {
   const QUERY = `INSERT INTO questions_table (question, choices, answer) VALUES (?, ?, ?)`;
   try {
     const db = await db_pool.getConnection();
-    const res = db.query(QUERY, [question, choices, answer]);
+    const choicesJSON = JSON.stringify(choices); // need to parse choices array to JSON string before query
+    const res = await db.query(QUERY, [question, choicesJSON, answer]);
+    return res;
+  } catch (error) {
+    console.error(
+      "Query error occured when creating new question record in database: ",
+      error,
+    );
+    throw error;
+  }
+};
+
+export const questions_update = async (id, question, choices, answer) => {
+  const QUERY = `UPDATE questions_table SET question = ?, choices = ?, answer = ? WHERE id = ?`;
+  try {
+    const db = await db_pool.getConnection();
+    const choicesJSON = JSON.stringify(choices); // need to parse choices array to JSON string before query
+    const res = await db.query(QUERY, [question, choicesJSON, answer, id]);
+    return res;
+  } catch (error) {
+    console.error(
+      "Query error occured when creating new question record in database: ",
+      error,
+    );
+    throw error;
+  }
+};
+
+export const questions_delete = async (id) => {
+  const QUERY = `INSERT INTO questions_table (question, choices, answer) VALUES (?, ?, ?)`;
+  try {
+    const db = await db_pool.getConnection();
+    const choicesJSON = JSON.stringify(choices); // need to parse choices array to JSON string before query
+    const res = await db.query(QUERY, [question, choicesJSON, answer]);
     return res;
   } catch (error) {
     console.error(
