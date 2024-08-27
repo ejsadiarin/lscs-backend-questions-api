7d884bc72ca5037603dc74c3968f3d5cf99298b6
ejsadiarin <ekwtech.container720@passinbox.com>
2024-08-28 01:40:23 +0800

    feat: create service handlers

---
+++ handlers/index.js
@@ -0,0 +1,6 @@
+export const createQuestion = async (req, res) => {};
+export const updateQuestion = async (req, res) => {};
+export const deleteQuestion = async (req, res) => {};
+export const getQuestion = async (req, res) => {};
+export const getAllQuestion = async (req, res) => {};
+export const checkAnswer = async (req, res) => {};
