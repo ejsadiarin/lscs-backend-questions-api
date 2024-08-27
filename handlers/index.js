7213ab4e94286e4442e4d1bec4297e862fe864d6
ejsadiarin <ekwtech.container720@passinbox.com>
2024-08-28 02:58:18 +0800

    feat(handlers): create handler serv

--- handlers/index.js
+++ handlers/index.js
@@ -1,6 +1,76 @@
-export const createQuestion = async (req, res) => {};
-export const updateQuestion = async (req, res) => {};
-export const deleteQuestion = async (req, res) => {};
-export const getQuestion = async (req, res) => {};
-export const getAllQuestion = async (req, res) => {};
-export const checkAnswer = async (req, res) => {};
+import {
+  questions_create,
+  questions_getAllQuestions,
+  questions_getQuestionById,
+  questions_update,
+} from "../db/query.js";
+
+export const createQuestion = async (req, res) => {
+  const { question, choices, answer } = req.body;
+
+  if (!question || !choices || !answer)
+    return res.status(403).json({ message: "No input parameters provided." });
+
+  try {
+    const data = await questions_create(question, choices, answer);
+    return res.status(201).json({ data });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Error occured when creating new question." });
+  }
+};
+
+export const updateQuestion = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await questions_update(id);
+    return res.status(200).json({ data }); // better if maybe .send(...)?
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error occured when checking answer." });
+  }
+};
+
+export const deleteQuestion = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await deleteQuestion(id);
+    return res.status(200).json({ data });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error occured when checking answer." });
+  }
+};
+
+export const getQuestionById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const data = await questions_getQuestionById(id);
+    return res.status(200).json({ data });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error occured when creating question." });
+  }
+};
+
+export const getAllQuestions = async (req, res) => {
+  try {
+    const data = await questions_getAllQuestions();
+    return res.status(200).json({ data });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Error occured when getting all questions." });
+  }
+};
+
+export const checkAnswer = async (req, res) => {
+  try {
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error occured when checking answer." });
+  }
+};
