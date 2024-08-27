d92e6b3eded70cf83d58484dba68d779974262de
ejsadiarin <ekwtech.container720@passinbox.com>
2024-08-28 01:40:36 +0800

    feat: init db connection

---
+++ db/index.js
@@ -0,0 +1,20 @@
+// init connections to mysql db
+import { createPool } from "mysql2/promise";
+
+export const db_pool = createPool({
+  name: process.env.MYSQL_DB_NAME,
+  password: process.env.MYSQL_DB_PASSWORD,
+  host: process.env.MYSQL_DB_HOST,
+  user: process.env.MYSQL_DB_USER,
+  port: process.env.MYSQL_DB_PORT,
+});
+
+export const initDBConnection = async () => {
+  try {
+    await db_pool.getConnection();
+    console.log("Successfully connected to MYSQL DB!");
+  } catch (error) {
+    console.error("Cannot connect to MYSQL DB: ", error);
+    throw error;
+  }
+};
