dcf32afc431dab6448c6a1164223ccaefc7d4a02
ejsadiarin <ekwtech.container720@passinbox.com>
2024-08-28 19:52:26 +0800

    chore(refactor): abstracted hhiheih

--- routes/index.js
+++ routes/index.js
@@ -1,105 +1,29 @@
 import { Router } from "express";
-import { Question } from "../db/schema.js";
+import {
+  checkAnswer,
+  createQuestion,
+  deleteQuestion,
+  getAllQuestions,
+  getQuestionById,
+  updateQuestion,
+} from "../handlers/question-service.js";
 
 export const questionRouter = Router();
 
-// test
+// test route only
 questionRouter.get("/", (req, res) => {
   console.log("Connected");
   return res.status(200).send({ message: "Success" });
 });
 
-// add question to db
-// -> req: must be json of whole schema
-questionRouter.post("/create", async (req, res) => {
-  try {
-    const { question, choices, answer } = req.body;
-    if (choices.length < 2) {
-      return res
-        .status(400)
-        .json({ message: "At least two choices are required" });
-    }
-    const newQuestion = new Question({ question, choices, answer });
-    await newQuestion.save();
-    res.status(201).json(newQuestion);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+questionRouter.post("/create", createQuestion);
 
-// edit question and update db
-// -> req: must be json of whole schema
-questionRouter.put("/update/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const updatedQuestion = await Question.findByIdAndUpdate(id, req.body, {
-      new: true,
-    });
-    if (!updatedQuestion) {
-      return res.status(404).json({ message: "Question not found" });
-    }
-    res.status(200).json(updatedQuestion);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+questionRouter.put("/update/:id", updateQuestion);
 
-// delete question and update db
-questionRouter.delete("/delete/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const deletedQuestion = await Question.findByIdAndDelete(id);
-    if (!deletedQuestion) {
-      return res.status(404).json({ message: "Question not found" });
-    }
-    res.status(200).json({ message: "Question deleted" });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+questionRouter.delete("/delete/:id", deleteQuestion);
 
-// get question data (list details of specified question)
-questionRouter.get("/get/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const question = await Question.findById(id);
-    if (!question) {
-      return res.status(404).json({ message: "Question not found" });
-    }
-    res.status(200).json(question);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+questionRouter.get("/get/:id", getQuestionById);
 
-// list all questions
-questionRouter.get("/list", async (req, res) => {
-  try {
-    const questions = await Question.find();
-    res.status(200).json(questions);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+questionRouter.get("/list", getAllQuestions);
 
-// pass choice to question
-// -> req: must be json "choice": String
-// -> check if correct, wrong, or invalid choice (doesn't exist in choice)
-// -> return 200 if correct and wrong, 404 invalid
-questionRouter.get("/check-answer/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { choice } = req.body;
-    const question = await Question.findById(id);
-    if (!question) {
-      return res.status(404).json({ message: "Question not found" });
-    }
-    if (!question.choices.includes(choice)) {
-      return res.status(400).json({ message: "Invalid choice" });
-    }
-    const isCorrect = choice === question.answer; // bool
-    res.status(200).json({ correct: isCorrect });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+questionRouter.get("/check-answer/:id", checkAnswer);
